32c594aca9044547e7c0aca77c11206cec70bf3e
Karina Pleskach <karinapleskach@mail.ru>
2020-04-14 17:19:40 +0300

    g

--- src/main/java/com/bsuir/rpodmp/bonup/controller/TestController.java
+++ src/main/java/com/bsuir/rpodmp/bonup/controller/TestController.java
@@ -1,11 +1,17 @@
 package com.bsuir.rpodmp.bonup.controller;
 
+import com.bsuir.rpodmp.bonup.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/test")
     @ResponseBody
     public String test() {
@@ -16,5 +22,11 @@ public class TestController {
     @ResponseBody
     public String hello() {
         return "Hello!";
+    }
+
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public String getCurrentUser() {
+return userService.getCurrentUserMail();
     }
 }
